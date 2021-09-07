378190c0a556d2b953ae16c034d3cecede991855
Alexey Voroyev <agvorobyev@sberbank.ru>
2021-09-07 14:44:42 +0300

    files pj

---
+++ src/com/sparrowww/sololearn/java/Main.java
@@ -0,0 +1,9 @@
+package com.sparrowww.sololearn.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+    }
+}
